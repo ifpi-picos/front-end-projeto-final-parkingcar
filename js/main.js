83fce0c35a813884a4e7095967089835a9009d3e
Matheus Santana de Sousa <matheussantana73@hotmail.com>
2019-12-29 17:51:20 -0300

    Problema com a deteccao da url para marcar o item do menu resolvido

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function ChangeItemMenuSelected() {
 	const liSelected = document.getElementsByClassName('item--menu')[0].children;
-	const hrefLocation = document.location.href;
+	const url = document.location.origin+document.location.pathname;
 	for (let index = 0; index < liSelected.length; index++) {
 		const aHref = liSelected[index].children[0].href;
-		if (aHref === hrefLocation) {
+		if (aHref === url) {
 			liSelected[index].className = 'item--menu--selected';
 			break;
 		}
