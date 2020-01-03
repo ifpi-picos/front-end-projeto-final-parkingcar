942fea385ec38cfdee5706b29abb23af00200f7c
Matheus Santana de Sousa <matheussantana73@hotmail.com>
2020-01-03 12:11:59 -0300

    Front end do login redirecionando para o usuario adequado (FAKE)

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+function changeEndPoint() {
+    const auth = {
+        username: document.getElementById('login-username').value,
+        password: document.getElementById('login-password').value,
+    }
+    if (auth.username === 'Admin') {
+        location.href = '/pages/admin/dashboard.html'
+    } else {
+
+        location.href = '/pages/funcionario/form/entrada-saida.html';
+    }
+}
+
+document.addEventListener('submit', event => {
+    event.preventDefault();
+    changeEndPoint();
+});
